edcb93430408db332377d8bc81aed0a53b001f8f
Parth <adokar.parth@gmail.com>
2023-06-22 16:48:53 +0530

    more calculators and faq

--- resources/js/main.js
+++ resources/js/main.js
@@ -37,47 +37,6 @@ function toggleAccordion() {
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
-// Api
-
-// c5eed1ea747dbcef1a182debf55dd0d57cb72051
-
-// function fetchArticles() {
-//   fetch('https://wger.de/api/v2/article')
-//     .then(response => response.json())
-//     .then(data => {
-//       // Process the fetched articles
-//       data.results.forEach(article => {
-//         console.log('Title:', article.title);
-//         console.log('URL:', article.url);
-//         console.log('------------------------');
-//       });
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//     });
-// }
-
-// // Function to fetch guides
-// function fetchGuides() {
-//   fetch('https://wger.de/api/v2/exerciseinfo')
-//     .then(response => response.json())
-//     .then(data => {
-//       // Process the fetched guides
-//       data.results.forEach(guide => {
-//         console.log('Name:', guide.name);
-//         console.log('Description:', guide.description);
-//         console.log('------------------------');
-//       });
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//     });
-// }
-
-// // Fetch articles and guides
-// fetchArticles();
-// fetchGuides();
-
 // BMI
 
 // select the calculate button
@@ -120,5 +79,63 @@ function showResult(bmi) {
 
   // Body Fat 
 
+  function calculateMaleBodyFat() {
+    let height = document.getElementById("maleHeight").value;
+    let weight = document.getElementById("maleWeight").value;
+    let waist = document.getElementById("maleWaist").value;
+    let neck = document.getElementById("maleNeck").value;
+
+    let bodyFatPercentage = 495 / (1.0324 - 0.19077 * Math.log10(waist - neck) + 0.15456 * Math.log10(height)) - 450;
+    let leanBodyMass = weight - (weight * bodyFatPercentage / 100);
+
+    document.getElementById("maleResult").innerHTML = "Estimated body fat percentage: " + bodyFatPercentage.toFixed(2) + "%<br>";
+    document.getElementById("maleResult").innerHTML += "Lean body mass: " + leanBodyMass.toFixed(2) + " kg";
+  }
+
+  function calculateFemaleBodyFat() {
+    let height = document.getElementById("femaleHeight").value;
+    let weight = document.getElementById("femaleWeight").value;
+    let waist = document.getElementById("femaleWaist").value;
+    let neck = document.getElementById("femaleNeck").value;
+    let hip = document.getElementById("femaleHip").value;
+
+    let bodyFatPercentage = 495 / (1.29579 - 0.35004 * Math.log10(waist + hip - neck) + 0.22100 * Math.log10(height)) - 450;
+    let leanBodyMass = weight - (weight * bodyFatPercentage / 100);
+
+    document.getElementById("femaleResult").innerHTML = "Estimated body fat percentage: " + bodyFatPercentage.toFixed(2) + "%<br>";
+    document.getElementById("femaleResult").innerHTML += "Lean body mass: " + leanBodyMass.toFixed(2) + " kg";
+  }
+
   // Calorie Intake
  
+  function calculateCalorieIntake() {
+    let weight = parseFloat(document.getElementById("weight").value);
+    let height = parseFloat(document.getElementById("height").value);
+    let age = parseInt(document.getElementById("age").value);
+    let gender = document.getElementById("gender").value;
+    let activityLevel = document.getElementById("activityLevel").value;
+
+    let calorieIntake;
+
+    if (gender.toLowerCase() === "male") {
+      calorieIntake = 10 * weight + 6.25 * height - 5 * age + 5;
+    } else if (gender.toLowerCase() === "female") {
+      calorieIntake = 10 * weight + 6.25 * height - 5 * age - 161;
+    } else {
+      console.log("Invalid gender input.");
+      return;
+    }
+
+    if (activityLevel.toLowerCase() === "sedentary") {
+      calorieIntake *= 1.2;
+    } else if (activityLevel.toLowerCase() === "moderate") {
+      calorieIntake *= 1.55;
+    } else if (activityLevel.toLowerCase() === "active") {
+      calorieIntake *= 1.9;
+    } else {
+      console.log("Invalid activity level input.");
+      return;
+    }
+
+    document.getElementById("calorieIntakeResult").innerHTML = "Your estimated daily calorie intake is: " + calorieIntake.toFixed(2) + " calories.";
+  }
